b910f8a682f2127415ff36516ffda8094450002e
Samuel Madrid <madridossa@gmail.com>
2024-06-28 13:02:05 -0500

    Added ENV port to publish

--- Models/server.js
+++ Models/server.js
@@ -7,7 +7,7 @@ class Server{
     constructor(){
 
         this.app = express();
-        this.port = 8080;
+        this.port = process.env.PORT;
 
         //Conexion base de datos
         this.conectarDB();
